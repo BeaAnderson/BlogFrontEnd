bd2606615583f5900b59b6604426ddd2f5a1a950
bea.anderson <bea.anderson@fdmgroup.com>
2023-08-30 13:10:06 +0100

    add blog

--- src/pages/CreateBlogPage.js
+++ src/pages/CreateBlogPage.js
@@ -1,7 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const CreateBlogPage = () => {
-  return <div>CreateBlogPage</div>;
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState("");
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token")
+  const [userLoggedIn, setUserLoggedIn] = useState([]);
+
+  useEffect(()=>{
+    const verifyUser = async () => {
+        try {
+            let response = await axios.get("http://localhost:8088/api/v1/verify",{
+                headers: {
+                  'Authorization': `Bearer ${token}`
+                }
+              }
+            );
+            setUserLoggedIn(true);
+        } catch (error) {
+            if (error.response.status === 401){
+                setUserLoggedIn(false);
+            }
+        }
+    };
+    verifyUser();
+  }, []);
+
+  const addBlogs = async(
+    user,
+    title,
+    body
+  ) => {
+    let response = await axios.post("http://localhost:8088/api/v1/blogs", {
+      user: { userId: user },
+      title: title,
+      body: body,
+    },{
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
+    setBlogs((blogs) => [response.data, ...blogs]);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addBlogs(user,title,body);
+    navigate("/");
+  }
+  
+  if (userLoggedIn){
+  return (
+  <div>
+    <h1>Create blog</h1>
+    <form className="addBlogForm" onSubmit={handleSubmit}>
+    <label>User Id:</label><br/>
+      <input type="text" id="user" value={user} name="userId" onChange={(e) => setUser(e.target.value)}/><br/>
+      <label>Title:</label><br/>
+      <input type="text" id="title" value={title} name="blogTitle" onChange={(e) => setTitle(e.target.value)}/><br/>
+      <label>Body:</label><br/>
+      <textarea type="text" id="body" value={body} name="blogBody" rows="10" cols="100" onChange={(e) => setBody(e.target.value)}/><br/>
+      <input type="submit" value="submit"/>
+    </form>
+  </div>)} else {
+    navigate("/accessdenied");
+  }
 };
 
 export default CreateBlogPage;
